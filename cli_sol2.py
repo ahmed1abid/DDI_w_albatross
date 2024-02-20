bab08839d2ac868d0c938a38dd74af213780eca6
unknown <aabid@enssat.fr>
2024-02-20 10:09:46 +0100

    add debug mode on the CLI

--- cli_sol2.py
+++ cli_sol2.py
@@ -14,7 +14,6 @@ ssn = None
 # Connect to the local Ethereum node
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545', request_kwargs={'timeout': 10000}))
 
-
 # Load contract ABI and address from JSON file
 with open("./build/contracts/DataRegistry.json", "r") as json_file:
     contract_data = json.load(json_file)
@@ -23,20 +22,26 @@ with open("./build/contracts/DataRegistry.json", "r") as json_file:
 contract_abi_1 = contract_data["abi"]
 contract_address_1 = '0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B'
 private_key_1 = "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d"
+
 # Create a contract instance
 data_registry = w3.eth.contract(address=contract_address_1, abi=contract_abi_1)
-# Load contract ABI and address
 
+# Load contract ABI and address
 with open("./build/contracts/VMController.json", "r") as json_file:
     contract_data = json.load(json_file)
-contract_address="0xCfEB869F69431e42cdB54A4F4f105C19C080A601"
+
+contract_address = "0xCfEB869F69431e42cdB54A4F4f105C19C080A601"
 contract_abi = contract_data["abi"]
 vm_controller = w3.eth.contract(address=contract_address, abi=contract_abi)
+
 # Set a sample user address for testing
 user_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
 
 
 def initialize_user():
+    """
+    Initialize user data.
+    """
     global name, wallet_file, user, ssn
 
     # Get user input
@@ -51,7 +56,6 @@ def initialize_user():
     print("Enter your data: ", end="")
     data = input()
 
-
     try:
         # Create User instance and encrypt data
         user = User(name=name, ssn=ssn, data=data)
@@ -62,7 +66,21 @@ def initialize_user():
         print("Error in loading wallet")
         raise e
 
+
 async def send_transaction_async(chunk, nonce, gas_price, gas_limit, chain_id):
+    """
+    Asynchronously send a transaction to the blockchain.
+
+    Args:
+        chunk (bytes): Data chunk to send.
+        nonce (int): Transaction nonce.
+        gas_price (int): Gas price for the transaction.
+        gas_limit (int): Gas limit for the transaction.
+        chain_id (int): Chain ID.
+
+    Returns:
+        dict: Response from the blockchain node.
+    """
     transaction = {
         'to': user_address,
         'gas': gas_limit,
@@ -81,7 +99,14 @@ async def send_transaction_async(chunk, nonce, gas_price, gas_limit, chain_id):
     response = requests.post('http://127.0.0.1:8545', json=payload)
     return response.json().get('result')
 
+
 async def send_file_to_blockchain(file_path):
+    """
+    Send a file to the blockchain.
+
+    Args:
+        file_path (str): Path to the file to send.
+    """
     if not file_path:
         print("File path not provided.")
         return
@@ -107,20 +132,22 @@ async def send_file_to_blockchain(file_path):
         end = (i + 1) * chunk_size if i < 99 else len(file_content)
         chunk = file_content[start:end]
 
-        # Print the generated transaction data
-
         tasks.append(send_transaction_async(chunk, nonce + i, gas_price, gas_limit, chain_id))
 
-    # Execute all tasks asynchronously
     responses = await asyncio.gather(*tasks)
 
-    # Wait for transactions to be mined
     await wait_for_transactions_mined(responses)
 
     print("All transactions sent to the blockchain successfully.")
 
 
 async def wait_for_transactions_mined(tx_hashes):
+    """
+    Wait for transactions to be mined.
+
+    Args:
+        tx_hashes (list): List of transaction hashes to monitor.
+    """
     for tx_hash in tx_hashes:
         if tx_hash is None:
             continue
@@ -138,6 +165,15 @@ async def wait_for_transactions_mined(tx_hashes):
 
 
 def is_contract_deployed(address):
+    """
+    Check if a contract is deployed at the given address.
+
+    Args:
+        address (str): Address of the contract to check.
+
+    Returns:
+        bool: True if a contract is deployed, False otherwise.
+    """
     try:
         code = w3.eth.get_code(address)
         return code and code != "0x"
@@ -146,18 +182,20 @@ def is_contract_deployed(address):
         return False
 
 
-
 async def get_latest_transaction_hashes():
+    """
+    Get the latest transaction hashes.
+
+    Returns:
+        list: List of transaction hashes.
+    """
     try:
         transaction_hashes = []
 
         for latest_block_number in range(0, 101):
-            # Get transactions from the latest block
             block = w3.eth.get_block(latest_block_number, True)
 
-
             if block and 'transactions' in block:
-                # Extract all transaction hashes from the block
                 transaction_hashes.extend([tx['hash'].hex() for tx in block['transactions']])
             else:
                 print(f"No transactions found in block {latest_block_number}.")
@@ -170,9 +208,11 @@ async def get_latest_transaction_hashes():
 
 
 async def get_file_from_blockchain():
+    """
+    Get a file from the blockchain.
+    """
     transaction_hashes = await get_latest_transaction_hashes()
     for tx_hash in transaction_hashes:
-        # JSON-RPC request payload
         payload = {
             'jsonrpc': '2.0',
             'method': 'eth_getTransactionByHash',
@@ -181,18 +221,15 @@ async def get_file_from_blockchain():
         }
 
         try:
-            # Make the JSON-RPC request
             response = requests.post('http://127.0.0.1:8545', json=payload)
-            response.raise_for_status()  # Raise an exception for HTTP errors
+            response.raise_for_status()
             result = response.json().get('result')
 
             if result:
-                # Extract the input data from the transaction
                 input_data = result.get('input')
                 decoded_input_data = bytes.fromhex(input_data[2:])
-                file_path = "unipi.spt"  # Generate a unique file name
+                file_path = "./unipi.hvt"  # Adjust path as needed
 
-                # Write the binary data to a file
                 with open(file_path, "ab") as file:
                     file.write(decoded_input_data)
 
@@ -202,83 +239,100 @@ async def get_file_from_blockchain():
         except requests.exceptions.RequestException as e:
             print(f"Error during JSON-RPC request: {e}")
 
-async def Destroy_vm(vm_name) : 
-        global w3, vm_controller, user_address
 
-        # Get nonce and chain ID
-        nonce = w3.eth.get_transaction_count(user_address)
-        chain_id = w3.eth.chain_id
+async def Destroy_vm(vm_name="unipi"):
+    """
+    Destroy a virtual machine.
 
-        # Encode the function call
-        transaction = vm_controller.functions.destroyVM(
-            vm_name).build_transaction({
-            'gas': 2000000,
-            'gasPrice': 20000000000,
-            'from': user_address,
-            'nonce': nonce,
-            'chainId': chain_id
-        })
+    Args:
+        vm_name (str): Name of the virtual machine.
+    """
+    global w3, vm_controller, user_address
 
-        # Send transaction
-        tx_hash = w3.eth.send_transaction(transaction)
-        print(f"VM is getting destroyed. Transaction Hash: {tx_hash.hex()}")
+    nonce = w3.eth.get_transaction_count(user_address)
+    chain_id = w3.eth.chain_id
+
+    transaction = vm_controller.functions.destroyVM(
+        vm_name).build_transaction({
+        'gas': 2000000,
+        'gasPrice': 20000000000,
+        'from': user_address,
+        'nonce': nonce,
+        'chainId': chain_id
+    })
+
+    tx_hash = w3.eth.send_transaction(transaction)
+    print(f"VM is getting destroyed. Transaction Hash: {tx_hash.hex()}")
 
 
-async def Create_vm_with_unipi(vm_name,hook,ipv4,
-            ipv4_gateway,
-            port,
-            tls,
-            ssh_key,
-            ) : 
-        await get_file_from_blockchain()
-        global w3, vm_controller, user_address
+async def Create_vm_with_unipi(vm_name, hook, ipv4, ipv4_gateway, port, tls):
+    """
+    Create a virtual machine with UNIPI.
 
-        # Get nonce and chain ID
-        nonce = w3.eth.get_transaction_count(user_address)
-        chain_id = w3.eth.chain_id
+    Args:
+        vm_name (str): Name of the virtual machine.
+        hook (str): Hook for the virtual machine.
+        ipv4 (str): IPv4 address for the virtual machine.
+        ipv4_gateway (str): IPv4 gateway for the virtual machine.
+        port (int): Port for the virtual machine.
+        tls (bool): Whether to enable TLS for the virtual machine.
+    """
+    await get_file_from_blockchain()
+    global w3, vm_controller, user_address
 
-        # Encode the function call
-        transaction = vm_controller.functions.createVM(
-            vm_name,
-            "https://github.com/ahmed1abid/Test.git",
-            hook,
-            ipv4,
-            ipv4_gateway,
-            port,
-            tls,
-            ssh_key,
-        ).build_transaction({
-            'gas': 2000000,
-            'gasPrice': 20000000000,
-            'from': user_address,
-            'nonce': nonce,
-            'chainId': chain_id
-        })
+    nonce = w3.eth.get_transaction_count(user_address)
+    chain_id = w3.eth.chain_id
 
-        # Send transaction
-        tx_hash = w3.eth.send_transaction(transaction)
-        print(f"VM creation initiated. Transaction Hash: {tx_hash.hex()}")
+    transaction = vm_controller.functions.createVM(
+        vm_name,
+        "https://github.com/ahmed1abid/Test.git",
+        hook,
+        ipv4,
+        ipv4_gateway,
+        port,
+        tls,
+        "ed25519:2JueTxGu7icIG6jpfFDl4AEr4L6zTUbMkS+e2vW4B/8="
+    ).build_transaction({
+        'gas': 2000000,
+        'gasPrice': 20000000000,
+        'from': user_address,
+        'nonce': nonce,
+        'chainId': chain_id
+    })
 
-async def sign_vm(vm_name="unipi") : 
-        global w3, vm_controller, user_address
+    tx_hash = w3.eth.send_transaction(transaction)
+    print(f"VM creation initiated. Transaction Hash: {tx_hash.hex()}")
 
-        # Get nonce and chain ID
-        nonce = w3.eth.get_transaction_count(user_address)
-        chain_id = w3.eth.chain_id
 
-        # Encode the function call
-        transaction = vm_controller.functions.signVM(
-            vm_name).build_transaction({
-            'gas': 2000000,
-            'gasPrice': 20000000000,
-            'from': user_address,
-            'nonce': nonce,
-            'chainId': chain_id
-        })
+async def sign_vm(vm_name="unipi", debug_mode=False):
+    """
+    Sign a virtual machine.
 
-        # Send transaction
-        tx_hash = w3.eth.send_transaction(transaction)
-        print(f"VM signement initiated. Transaction Hash: {tx_hash.hex()}")
+    Args:
+        vm_name (str): Name of the virtual machine.
+        debug_mode (bool): Whether to enable debug mode.
+    """
+    global w3, vm_controller, user_address
+
+    nonce = w3.eth.get_transaction_count(user_address)
+    chain_id = w3.eth.chain_id
+
+    transaction = vm_controller.functions.signVM(
+        vm_name).build_transaction({
+        'gas': 2000000,
+        'gasPrice': 20000000000,
+        'from': user_address,
+        'nonce': nonce,
+        'chainId': chain_id
+    })
+
+    tx_hash = w3.eth.send_transaction(transaction)
+    print(f"VM signement initiated. Transaction Hash: {tx_hash.hex()}")
+
+    if debug_mode:
+        print("Debug mode enabled.")
+
+
 if __name__ == "__main__":
     # Enter the main loop
     while True:
@@ -290,18 +344,16 @@ if __name__ == "__main__":
             if inp_list[1] in ['user']:
                 initialize_user()
 
-
         elif inp_list[0] in ['destroy']:
             if inp_list[1] in ['vm']:
                 asyncio.run(Destroy_vm("unipi"))
             else:
-                print("there si a problem with the vm : either it's not creater or we dont have acces to it to delete it")
-
+                print("There is a problem with the VM: either it's not created or we don't have access to delete it.")
 
         elif inp_list[0] in ['send', 'store', 'upload']:
             if inp_list[1] in ['data']:
                 loop = asyncio.get_event_loop()
-                loop.run_until_complete(send_file_to_blockchain('/home/ahmed/Desktop/cyber/idenity/unipi/dist/unipi.spt'))
+                loop.run_until_complete(send_file_to_blockchain('./unipi.hvt'))  # Adjust path as needed
             else:
                 print("User address is not set. Make sure to set it to your Ethereum address.")
 
@@ -311,7 +363,8 @@ if __name__ == "__main__":
                 loop.run_until_complete(get_file_from_blockchain())
         elif inp_list[0] in ['sign']:
             if inp_list[1] in ['vm']:
-                asyncio.run(sign_vm("unipi"))
+                debug_mode = '--debug' in inp_list  # Check if debug mode is enabled
+                asyncio.run(sign_vm("unipi", debug_mode=debug_mode))
         elif inp_list[0] in ['start']:
             if inp_list[1] in ['tls']:
                 asyncio.run(Certif.server_start_endpoint())
@@ -320,11 +373,9 @@ if __name__ == "__main__":
                 asyncio.run(Certif.user_add_policy())
         elif inp_list[0] in ['sign']:
             if inp_list[1] in ['certif']:
-             Certif.sign_certificate_request()
+                Certif.sign_certificate_request()
         elif inp_list[0] in ['create']:
             if inp_list[1] in ['vm']:
-                asyncio.run(Create_vm_with_unipi("unipi","/updatewebhook","10.0.0.10/24","10.0.0.254",8443,True,"ed25519:2JueTxGu7icIG6jpfFDl4AEr4L6zTUbMkS+e2vW4B/8="))
-
-
+                asyncio.run(Create_vm_with_unipi("unipi", "/updatewebhook", "10.0.0.10/24", "10.0.0.254", 8443, True))
             else:
                 print("User address is not set. Make sure to set it to your Ethereum address.")
